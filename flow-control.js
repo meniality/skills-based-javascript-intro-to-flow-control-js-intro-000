4065b64afecdecae9bbbfe2034d0cfacbf3aad76
patrick hoehn <patrh_99@hotmail.com>
2019-09-28 18:07:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,11 +6,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if (age>=13 && <=19) {
-    return (You are a teenager!)
-  } else {return ("You are not a teenager")
-  }
-
+  
 }
 
 function ageChecker(age) {
