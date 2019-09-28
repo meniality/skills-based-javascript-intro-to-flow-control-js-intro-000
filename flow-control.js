ddebfc55c7575a27a60365fa568f30ffd3fc3835
patrick hoehn <patrh_99@hotmail.com>
2019-09-28 18:43:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,7 @@ function switchAge(age) {
     case age>=13 && age<=19:
       return "You are a teenager";
     default:
-      return "You have an age"
+      return "You have an age";
     }
   }
 
