fb7df5884e543f393c828d9104bd502bf1def1a3
patrick hoehn <patrh_99@hotmail.com>
2019-09-28 18:54:01 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,7 +25,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   if (age>=13 && age<=19) {
-    return ("You are a teenager")
+    return ("You have an age")
   } else if (age<13) {
     return ("You are a kid")
   } else return ("You have an age")
